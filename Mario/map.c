4db8f917185a7ae5950a405b313cea1d0dba5a85
Bodin Bizetic <dinbo99@gmail.com>
2019-05-14 19:35:31 +0200

    Dodao sam strukturu MAPa koja obuhvata matrice za AI i za Mapu (te dve stvari se uvek zajedno koriste)

---
+++ Mario/map.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "map.h"
+
+/*!
+*		\brief Initializes Map structure, fills it with "sky" and adds one leyer of floor
+*/
+Map* initMap() {
+	Map *map = malloc(sizeof(Map));
+	if (!map)
+		printf_s("Failed to Initialize Map");
+	for (int i = 0; i < MAP_HEIGHT - 1; i++)
+		for (int j = 0; j < MAP_WIDTH * MAP_SEGMENTS_NUMBER - 1; j++)
+			map->map_Matrix[i][j] = (i == MAP_HEIGHT - 2) ? floor : sky;
+	return map;
+}
+
+/*!
+*		\brief Deletes Map structure
+*		\param Map map Addres of map
+*/
+void  destroyMap(Map *map) {
+	free(map);
+}
---
+++ Mario/map.h
@@ -0,0 +1,28 @@
+#ifndef map_h
+#define map_h
+
+#define MAP_HEIGHT 15
+#define MAP_WIDTH 15
+#define MAP_SEGMENTS_NUMBER 10
+
+#define AI_NUMBER 9
+#define AI_MAX_NUMBER 10
+//Ako je malo slovo - podrazumeva mapId, a veliko aiId
+enum mapId { sky, floor, basic, question, hidden, pipetunnel, pipe, flag, castle, pikes, mario, projectile, shroom, star, turtle, devil, plantie };
+
+enum aiId {Brick, Question, Hidden, Projectile, Shroom, Star, Turtle, Devil, Plantie};
+
+/*!
+*		\brief Main structure that cantains all active AI's and all blocks on the map
+*/
+typedef struct MAP {
+	void *ai_Matrix[AI_NUMBER][AI_MAX_NUMBER];
+	int map_Matrix[MAP_HEIGHT][MAP_WIDTH * MAP_SEGMENTS_NUMBER];
+}Map;
+
+Map* initMap();
+
+void  destroyMap(Map *map);
+
+#endif
+
