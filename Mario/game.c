bb939fe9d1b26a1fe6d0bbacbcea72616447f3e0
Никола Маринковић <mn180342d@student.etf.bg.ac.rs>
2019-05-19 21:37:04 +0200

    iscrtavanje mape,radi,nece da prima komande i zatvara prozor posle 2 sekunde

--- Mario/game.c
+++ Mario/game.c
@@ -7,33 +7,52 @@
 #include "mario.h"
 #include "main_menu.h"
 
-#define dimension 16
+#define TILE_SIZE 16
 //DrawScreen crta na ekranu
-void drawScreen(SDL_Window *window, SDL_Renderer *renderer, Map *map, Mario *Mario) {
+void drawScreen(SDL_Window *window, SDL_Renderer *renderer, Map *map, Mario *mario) {
 	int i, j, x = 0, y = 0;
-	SDL_Rect rect = { 0,0,x,y };
+	SDL_Rect rect = { x,y,TILE_SIZE,TILE_SIZE };
+	SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
+	SDL_RenderClear(renderer);
 	for (i = 0; i < 10; i++) {
 		for (j = 0; j < 10; j++) {
 			//Belo nebo
+			rect.x = x;
+			rect.y = y;
 			if (map->map_Matrix[i][j] == 0) {
 				SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
 				SDL_RenderFillRect(renderer, &rect);
 			}
 			//Crn pod
-			else {
+			else  {
 				SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
 				SDL_RenderFillRect(renderer, &rect);
 			}
-			x += 16;
+			if (x == mario->coordinates.x && y == mario->coordinates.y) {
+				SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+				SDL_RenderFillRect(renderer, &rect);
+				SDL_SetRenderDrawColor(renderer, 100, 100, 100, 255);
+				SDL_RenderFillRect(renderer, &rect);
+				
+			}
+			x+= TILE_SIZE;
 		}
 		x = 0;
-		y += 16;
+		y += TILE_SIZE;
 	}
 	SDL_RenderPresent(renderer);
 }
 //igranje igre
-int Game(SDL_Window *window, SDL_Renderer *renderer, Map *map, Mario *Mario) {
+int Game(SDL_Window *window, SDL_Renderer *renderer, Map *map, Mario *mario) {
 	Map *mapa;
+	Mario *probni_mario;
+	probni_mario = malloc(sizeof(Mario));
+	probni_mario->size.x = TILE_SIZE;
+	probni_mario->size.y = TILE_SIZE;
+	probni_mario->coordinates.x = 3*TILE_SIZE;
+	probni_mario->coordinates.y = 3*TILE_SIZE;
+	probni_mario->speed.x = 0;
+	probni_mario->speed.y = 0;
 	mapa = malloc(sizeof(map));
 	int i, j, x = 0, y = 0;
 	for (i = 0; i < 10; i++)
@@ -46,22 +65,56 @@ int Game(SDL_Window *window, SDL_Renderer *renderer, Map *map, Mario *Mario) {
 
 	//Running for game loop
 	int Running = 1;
-	SDL_Event event;
+	SDL_Event eventgame;
 	while (Running) {
-		while (SDL_PollEvent(&event)) {
-			switch (event.type) {
+		while (SDL_PollEvent(&eventgame)) {
+			switch (eventgame.type) {
 			case SDL_QUIT:
 			case SDL_KEYDOWN:
-				switch (event.key.keysym.sym) {
+				switch (eventgame.key.keysym.sym) {
+				case SDLK_RIGHT:
+					mario->direction = 1;
+					if (mario->speed.x <= 16)
+						mario->speed.x += 2;
+					mario->coordinates.x += mario->speed.x;
+					break;
+				case SDLK_LEFT:
+					mario->direction = 0;
+					if (mario->speed.x >= -16)
+						mario->speed.x -= 2;
+					mario->coordinates.x += mario->speed.x;
+					break;
+				case SDLK_UP:
+					if (mario->speed.x  -16)
+						mario->speed.x -= 2;
+					mario->coordinates.x += mario->speed.x;
 				case SDLK_ESCAPE:
 					Running = 0;
 					return  0;
 					break;
 				}
-			case SDL_KEYUP:
+			case SDL_KEYUP: {
+				while (mario->speed.x>0) {
+					mario->speed.x -= 2;
+					mario->coordinates.x += mario->speed.x;
+				}
+				while (mario->speed.x < 0) {
+					mario->speed.x += 2;
+					mario->coordinates.x += mario->speed.x;
+				}
+				/*while (uslov da nije na podu) {
+					mario->speed.y -= 2;
+					mario->coordinates.y += mario->speed.y;
+				}
+				if (uslov da je na podu) {
+					mario->speed.y = 0;
+				}
+				*/
+
+			}
 			default: {}
 			}
-			drawScreen(window, renderer, mapa, mario);
 		}
+		drawScreen(window, renderer, mapa, probni_mario);
 	}
 }
