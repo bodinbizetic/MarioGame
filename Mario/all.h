f230b295ac9af5c309bd0c0350ed054ee709515d
bodinbizetic <bb180058@student.etf.bg.ac.rs>
2019-06-12 00:03:59 +0200

    all.h doc

--- Mario/all.h
+++ Mario/all.h
@@ -1,31 +1,73 @@
 #ifndef all_h
 #define all_h
 #include "SDL.h"
+
+/*!	\file all.h
+*	\brief Contains declarations and constants for all structures
+*/
+
+/*!
+*	\brief The smallest unit of mesurment
+*/
 #define EPSILON 1
 
+/*!
+*	\brief Width of score showing on the screen
+*/
 #define SCORE_WIDTH 100
+
+/*!
+*	\brief Heihgt of score showing on the screen
+*/
 #define SCORE_HEIGHT 60
 
+/*!
+*	\brief Number of blocks showing from the top to the bottom of the screen
+*/
 #define MAP_HEIGHT 15
+
+/*!
+*	\brief Number of blocks showing from the left to the right of screen
+*/
 #define MAP_WIDTH 15
+
+/*!
+*	\brief Number of segments which are dimension MAP_HEIGHT x MAP_WIDTH on the map
+*/
 #define MAP_SEGMENTS_NUMBER 20
 
+/*!
+*	\brief Number of demo map segment
+*/
 #define DEMO_MAP_SEGMENTS_NUMBER 5
 
+/*!
+*	\brief Number of all objects in the game
+*/
 #define AI_NUMBER 17
 #define AI_MAX_NUMBER 1000
 
+/*!
+*	\brief Variable representing if game is running
+*/
 static int Game_Running = 1;
+
+/*!
+*	\brief Variable representing if music is on or off
+*/
 int music;
 
 /*!
-*	\brief Contains pair of 2d coordinates
+*	\brief Contains vector of 2d coordinates
 */
 typedef struct PAIR_XY {
 	int x;
 	int y;
 }Pair_xy;
 
+/*!
+*	\brief Structure representing Mario
+*/
 typedef struct {
 
 	Pair_xy coordinates, size, speed, dimension;
