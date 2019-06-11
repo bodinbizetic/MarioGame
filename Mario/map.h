cd0066f8a35b617024de8a401345449f1794f392
bodinbizetic <bb180058@student.etf.bg.ac.rs>
2019-06-11 22:47:59 +0200

    map.h doc

--- Mario/map.h
+++ Mario/map.h
@@ -8,14 +8,35 @@
 
 
 //Izrazeno u % po bloku
+
+/*!	
+*	\brief Relative x dimension for pipe in %
+*/
 #define PIPE_WIDTH 80
+
+/*!
+*	\brief Relative y dimension for pipe in %
+*/
 #define PIPE_HEIGHT 20
+
+/*!
+*	\brief Relative position of plantie in %
+*/
 #define PLANTIE_POS 50
 
-//IZMENA!Zamenio sam floor sa ground jer je pravio problem sa fjom floor iz math.h,zamenio sam i u map.c
+/*!
+*	\brief Enum of all types of blocks and AIs
+*/
 enum mapId { sky, ground, basic, question, hidden, pipetunnel, pipe, flag, castle, pikes, mario_id, projectile, shroom, star, turtle, devil, plantie };
-//Gravity defying blocks
+
+/*!
+*	\brief Enum of all gravity-collidable objects
+*/
 static int gravity_Blocks[] = { ground, question, hidden, basic, pipe, pikes, flag };
+
+/*!
+*	\brief Enum of all AIs
+*/
 static int ai_id[] = { projectile, shroom, star, turtle, devil, plantie, flag };
 //enum aiId {Brick, Question, Hidden, Projectile, Shroom, Star, Turtle, Devil, Plantie};
 
@@ -26,14 +47,16 @@ Map* initMap(SDL_Texture *block_Texture[AI_NUMBER][5], int demo);
 Map* LoadMap();
 
 int updateMapItems(Map *map);
+
 void  destroyMap(Map *map);
+
 int spawnShroom(Map *map, Pair_xy coord, SDL_Texture *block_Texture[AI_NUMBER][5], int lifes);
+
 int spawnProjectile(Map *map, Pair_xy coord, int orientation, int speed, SDL_Texture *block_Texture[AI_NUMBER][5]);
+
 int spawnCoin(Map *map, Pair_xy coord, SDL_Texture *block_Texture[AI_NUMBER][5]);
+
 int spawnDeathDevil(Map *map, Pair_xy coord, SDL_Texture *animation);
-
-
-
 
 #endif
 
