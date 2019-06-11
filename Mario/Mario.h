d4be127c450082652f1dabd7375950730eed1265
bodinbizetic <bb180058@student.etf.bg.ac.rs>
2019-06-11 22:30:32 +0200

    mario.h doc

--- Mario/Mario.h
+++ Mario/Mario.h
@@ -3,13 +3,44 @@
 #include "ai.h"
 #include "map.h"
 #include "all.h"
+
+/*!	\file mario.c
+*	\brief Contains game playing declarations and Mario-related constants
+*/
+
+/*!	
+*	\brief Maximum value for Mario jump timer
+*/
 #define MAX_JUMP 25
+
+/*!
+*	\brief Maximum value for Mario immoratility timer
+*/
 #define MAX_IMORTAL 250
+
+/*!
+*	\brief Maximum value for Mario x speed
+*/
 #define MAXSPEED 8
+
+/*!
+*	\brief Maximum value for Mario's projectile timer
+*/
 #define PROJECTILE_TIMER 25
+/*!
+*	\brief Reduction in % for Mario's y size
+*/
 #define MARIO_SHRINK 80
+
+/*!
+*	\brief Reduction in % for Mario's x size
+*/
 #define MARIO_SHRINK_X 80
+
+/*!
+*	\brief Value of gravitational acceleration
+*/
 #define G 1
-//Update govori koja je komanda
+
 int updateMario(SDL_Window *window, SDL_Renderer *renderer, Map *map, Mario *mario,Pair_xy update, SDL_Texture *block_Texture[AI_NUMBER][5]);
 #endif
